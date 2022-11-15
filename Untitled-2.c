c99333fd160b3cfc2e59ad06957094f03852d8d2
Edgrant <edgrant.henderson@ui.ac.id>
2022-11-15 11:01:51 +0700

    15 nov

--- Untitled-2.c
+++ Untitled-2.c
@@ -1,43 +1,16 @@
 #include <stdio.h>
-#include <math.h>
 
-int cekganjil(int i){
-    if (i %2 == 1) 
-        return 1;
-    else
-        return 0;
-
-}
-int cekkubik(int i){
-    int anum; 
-    float akar;
-    akar=cbrt((double)i); 
-    anum=akar; 
-    if(anum==akar)
-        return 1;
-    else
-        return 0;
-}
 int main(){
-    int i;
+    int i, n;//i sebagai counter dan n menandakan counter sampai berapa
+    printf ("************************Please Enter Input************************\n"
+            "| \n"
+            "| enter loop ammount: ");
+    scanf ("%d", &n);
+    printf ("| \n"
+            "******************************************************************\n");
 
-    printf("Masukkan nilai input, tekan -1 untuk cancel\n");
-    do {
-        scanf("%d",  &i);
-
-         if (i != -1){ 
-                if(cekganjil(i) && cekkubik(i)){
-                printf("\nGanjil Kubik");
-                }
-                if(cekganjil(i)){
-                printf("\nGanjil");
-                }
-                if(cekkubik(i)){
-                printf("\nKubik");
-                }
-                }else {
-                printf("Tidak termasuk ganjil ataupun kubik\n");
-                }
-
-        }while (i != -1);
-        return 0;}
+    for (i = 0; i < n; i++) {
+        printf ("loop %d", n);
+    }
+    return 0;
+}
--- tes3.c
+++ tes3.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
 int main(){
-    printf("bisa 3");
+    int i, n;//i sebagai counter dan n menandakan counter sampai berapa
+    printf ("************************Please Enter Input************************\n"
+            "| \n"
+            "| enter repeat how many times : ");
+    scanf ("%d", &n);
+    printf ("| \n"
+            "******************************************************************\n");
+
+    for (i = 0; i < n; i++) {
+        printf ("%d\n", i);
+    }
+    return 0;
 }
